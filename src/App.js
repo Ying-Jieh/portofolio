58176f3168bbb8f7ecd9f86fb9269c1bc23da8ab
Ying-Jieh <chris8332558@gmail.com>
2024-03-31 03:32:56 -0700

    finish Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Homescreen';
+import Navbar from './Pages/Home/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            {/* You can add other pages here */}
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Fonnd</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
       <Home />
     </div>
   );
