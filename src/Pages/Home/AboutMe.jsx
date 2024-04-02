9de1eeb861c990fb6c3381882c3861077d162b3a
Ying-Jieh <chris8332558@gmail.com>
2024-04-02 08:19:28 -0700

    finish portfolio setction

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about-section">
+            <div className="about-section-img">
+                <img src="./img/about-me.png" alt="About Me"/>
+            </div>
+            <div className="hero-section-content-box about-section-box">
+                <div className="hero-section-box">
+                    <h1 className="skill-section-heading">About Me</h1>
+                </div>
+                <p className="hero-section-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corrupti debitis aperiam reprehenderit!
+                </p>
+                <p className="hero-section-description">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo quam accusantium quia labore sit blanditiis!
+                </p>
+            </div>
+        </section>
+    )
+}
